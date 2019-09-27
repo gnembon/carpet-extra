3d53931abbd748e32c2f331ecaaa633b6290fa42
gnembon <gnembonmc@gmail.com>
2019-09-27 10:24:08 -0500

    fix 1.15 redir

--- src/main/java/carpetextra/mixins/AbstractButtonBlock_variableWoodMixin.java
+++ src/main/java/carpetextra/mixins/AbstractButtonBlock_variableWoodMixin.java
@@ -6,9 +6,6 @@ import net.minecraft.block.AbstractButtonBlock;
 import net.minecraft.block.AbstractPressurePlateBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.class_4538;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.util.Hand;
-import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
@@ -18,12 +15,12 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 @Mixin(AbstractButtonBlock.class)
 public class AbstractButtonBlock_variableWoodMixin
 {
-    @Redirect(method = "activate", at = @At(
+    @Redirect(method = "method_21845", at = @At(
             value = "INVOKE",
             target = "Lnet/minecraft/block/AbstractButtonBlock;getTickRate(Lnet/minecraft/class_4538;)I"
     ))
     private int getCustomTickRate(AbstractButtonBlock abstractButtonBlock, class_4538 viewableWorld_1,
-                                  BlockState blockState_1, World world_1, BlockPos blockPos_1, PlayerEntity playerEntity_1, Hand hand_1, BlockHitResult blockHitResult_1)
+                                  BlockState blockState_1, World world_1, BlockPos blockPos_1)
     {
         if (!CarpetExtraSettings.variableWoodDelays)
         {
