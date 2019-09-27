8d7e75bed25d4091a6586e60987a7645a7ca1f02
gnembon <gnembonmc@gmail.com>
2019-09-27 10:18:58 -0500

    1.15 mapping fix

--- src/main/java/carpetextra/mixins/AbstractButtonBlock_variableWoodMixin.java
+++ src/main/java/carpetextra/mixins/AbstractButtonBlock_variableWoodMixin.java
@@ -5,11 +5,11 @@ import carpetextra.utils.WoodDelayMultipliers;
 import net.minecraft.block.AbstractButtonBlock;
 import net.minecraft.block.AbstractPressurePlateBlock;
 import net.minecraft.block.BlockState;
+import net.minecraft.class_4538;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.ViewableWorld;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -20,9 +20,9 @@ public class AbstractButtonBlock_variableWoodMixin
 {
     @Redirect(method = "activate", at = @At(
             value = "INVOKE",
-            target = "Lnet/minecraft/block/AbstractButtonBlock;getTickRate(Lnet/minecraft/world/ViewableWorld;)I"
+            target = "Lnet/minecraft/block/AbstractButtonBlock;getTickRate(Lnet/minecraft/class_4538;)I"
     ))
-    private int getCustomTickRate(AbstractButtonBlock abstractButtonBlock, ViewableWorld viewableWorld_1,
+    private int getCustomTickRate(AbstractButtonBlock abstractButtonBlock, class_4538 viewableWorld_1,
                                   BlockState blockState_1, World world_1, BlockPos blockPos_1, PlayerEntity playerEntity_1, Hand hand_1, BlockHitResult blockHitResult_1)
     {
         if (!CarpetExtraSettings.variableWoodDelays)
--- src/main/java/carpetextra/mixins/AbstractPressurePlateBlock_variableWoodMixin.java
+++ src/main/java/carpetextra/mixins/AbstractPressurePlateBlock_variableWoodMixin.java
@@ -4,8 +4,8 @@ import carpetextra.CarpetExtraSettings;
 import carpetextra.utils.WoodDelayMultipliers;
 import net.minecraft.block.AbstractPressurePlateBlock;
 import net.minecraft.block.BlockState;
+import net.minecraft.class_4538;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.ViewableWorld;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,9 +17,9 @@ public class AbstractPressurePlateBlock_variableWoodMixin
 
     @Redirect(method = "updatePlateState", at = @At(
             value = "INVOKE",
-            target = "Lnet/minecraft/block/AbstractPressurePlateBlock;getTickRate(Lnet/minecraft/world/ViewableWorld;)I"
+            target ="Lnet/minecraft/block/AbstractPressurePlateBlock;getTickRate(Lnet/minecraft/class_4538;)I"
     ))
-    private int getCustomTickRate(AbstractPressurePlateBlock abstractPressurePlateBlock, ViewableWorld viewableWorld_1,
+    private int getCustomTickRate(AbstractPressurePlateBlock abstractPressurePlateBlock, class_4538 viewableWorld_1,
                                   World world_1, BlockPos blockPos_1, BlockState blockState_1, int int_1)
     {
         if (!CarpetExtraSettings.variableWoodDelays)
