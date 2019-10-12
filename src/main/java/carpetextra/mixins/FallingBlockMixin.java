af45bc2099dbeccba14f4df13280ef9b007ebc6d
gnembon <gnembonmc@gmail.com>
2019-10-12 00:46:51 -0300

    fixed dragon egg breaking

--- src/main/java/carpetextra/mixins/FallingBlockMixin.java
+++ src/main/java/carpetextra/mixins/FallingBlockMixin.java
@@ -7,13 +7,16 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.DragonEggBlock;
 import net.minecraft.block.FallingBlock;
 import net.minecraft.entity.FallingBlockEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
+import net.minecraft.util.math.ChunkPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Random;
 
 @Mixin(FallingBlock.class)
 public abstract class FallingBlockMixin extends Block
@@ -32,39 +35,40 @@ public abstract class FallingBlockMixin extends Block
     }
     
     @SuppressWarnings("ConstantConditions")
-    @Inject(method = "tryStartFalling", at = @At("HEAD"), cancellable = true)
-    private void onTryStartFalling(World world, BlockPos pos, CallbackInfo ci)
+    @Inject(method = "scheduledTick", at = @At("HEAD"), cancellable = true)
+    private void onTryStartFalling(BlockState blockState_1, ServerWorld serverWorld_1, BlockPos blockPos_1, Random random_1, CallbackInfo ci)
     {
         if (CarpetExtraSettings.dragonEggBedrockBreaking && (FallingBlock)(Object)this instanceof DragonEggBlock)
         {
-            if (canFallThrough(world.getBlockState(pos.down())) && pos.getY() >= 0)
+            if (canFallThrough(serverWorld_1.getBlockState(blockPos_1.down(1))) && blockPos_1.getY() >= 0)
             {
-                if (!DragonEggBedrockBreaking.fallInstantly && world.isAreaLoaded(pos.add(-32, -32, -32),  pos.add(32, 32, 32)))
+                if (!DragonEggBedrockBreaking.fallInstantly &&
+                        serverWorld_1.getChunkManager().shouldTickChunk(new ChunkPos(blockPos_1)))
                 {
-                    if (!world.isClient)
+                    if (!serverWorld_1.isClient)
                     {
-                        FallingBlockEntity fallingBlockEntity_1 = new FallingBlockEntity(world, (double) pos.getX() + 0.5D, (double) pos.getY(), (double) pos.getZ() + 0.5D, world.getBlockState(pos));
+                        FallingBlockEntity fallingBlockEntity_1 = new FallingBlockEntity(serverWorld_1, (double) blockPos_1.getX() + 0.5D, (double) blockPos_1.getY(), (double) blockPos_1.getZ() + 0.5D, serverWorld_1.getBlockState(blockPos_1));
                         this.configureFallingBlockEntity(fallingBlockEntity_1);
-                        world.spawnEntity(fallingBlockEntity_1);
+                        serverWorld_1.spawnEntity(fallingBlockEntity_1);
                     }
                 }
                 else
                 {
-                    if (world.getBlockState(pos).getBlock() == this)
+                    if (serverWorld_1.getBlockState(blockPos_1).getBlock() == this)
                     {
-                        world.clearBlockState(pos, false);
+                        serverWorld_1.removeBlock(blockPos_1, false);
                     }
-                    
+
                     BlockPos blockPos;
     
-                    for (blockPos = pos.down(); canFallThrough(world.getBlockState(blockPos)) && blockPos.getY() > 0; blockPos = blockPos.down())
+                    for (blockPos = blockPos_1.down(1); canFallThrough(serverWorld_1.getBlockState(blockPos)) && blockPos.getY() > 0; blockPos = blockPos.down(1))
                     {
                         ;
                     }
                     
                     if (blockPos.getY() > 0)
                     {
-                        world.setBlockState(blockPos, this.getDefaultState());
+                        serverWorld_1.setBlockState(blockPos, this.getDefaultState());
                     }
                 }
             }
