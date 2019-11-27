0cf2f503e3b1cc28d50936c8522f4e584eaf6841
gnembon <gnembonmc@gmail.com>
2019-11-26 23:45:48 -0400

    yeet autoCraftingTable

--- src/main/java/carpetextra/CarpetExtraServer.java
+++ src/main/java/carpetextra/CarpetExtraServer.java
@@ -3,7 +3,6 @@ package carpetextra;
 import carpet.CarpetExtension;
 import carpet.CarpetServer;
 import carpetextra.commands.PingCommand;
-import carpetextra.utils.CraftingTableBlockEntity;
 import com.mojang.brigadier.CommandDispatcher;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.command.ServerCommandSource;
@@ -15,7 +14,6 @@ public class CarpetExtraServer implements CarpetExtension
 
     static
     {
-        CraftingTableBlockEntity.init();
         CarpetServer.manageExtension(new CarpetExtraServer());
     }
 
