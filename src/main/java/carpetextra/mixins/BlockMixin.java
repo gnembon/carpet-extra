8b35fa4f67ad3a7018c08f3620b84352fffeee65
whoImT <Timmi.Heeke@gmail.com>
2020-04-18 03:08:28 +0200

    carefulBreak fixed no sound + not increasing stats
    added pickUp-sound to carefulBreak + added pickUp-stats

--- src/main/java/carpetextra/mixins/BlockMixin.java
+++ src/main/java/carpetextra/mixins/BlockMixin.java
@@ -1,15 +1,22 @@
 package carpetextra.mixins;
 
+import carpet.CarpetServer;
 import carpetextra.CarpetExtraSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.FacingBlock;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.stat.Stats;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -31,9 +38,18 @@ public abstract class BlockMixin implements ItemConvertible {
 
             if (world instanceof ServerWorld) {
                 getDroppedStacks(state, (ServerWorld) world, pos, blockEntity, player, stack).forEach((itemStack) -> {
-                    if (!player.giveItemStack(itemStack)) {
+
+                    Item item = itemStack.getItem();
+                    int itemAmount = itemStack.getCount();
+                    if (player.inventory.insertStack(itemStack)) {
+
+                        world.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.ENTITY_ITEM_PICKUP, SoundCategory.PLAYERS, 0.2f, (CarpetServer.rand.nextFloat() - CarpetServer.rand.nextFloat()) * 1.4F + 2.0F);
+                        player.increaseStat(Stats.PICKED_UP.getOrCreateStat(item),itemAmount);
+
+                    }else{
                         dropStack(world, pos, itemStack);
                     }
+
                 });
             }
 
