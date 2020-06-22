e066af1342c40c90c360f1c8c5d6fb51694d5bd0
gnembon <gnembonmc@gmail.com>
2020-06-21 22:38:00 -0300

    moved away from main classes

--- src/main/java/carpetextra/mixins/CrashReport_noopMixin.java
+++ src/main/java/carpetextra/mixins/CrashReport_noopMixin.java
@@ -1,19 +1,18 @@
 package carpetextra.mixins;
 
 import carpetextra.CarpetExtraServer;
-import net.minecraft.client.main.Main;
+import net.minecraft.util.crash.CrashReport;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Main.class)
-public class Main_clientNoopMixin
+@Mixin(CrashReport.class)
+public class CrashReport_noopMixin
 {
-    @Inject(method = "main", at = @At("HEAD"))
-    private static void onServerStarted(String[] args, CallbackInfo ci)
+    @Inject(method = "initCrashReport", at = @At("HEAD"))
+    private static void gameStarted(CallbackInfo ci)
     {
         CarpetExtraServer.noop();
     }
 }
-
