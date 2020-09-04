ebaab6e9528beea0c72865fc47d9d6394b2eabc7
whoImT <Timmi.Heeke@gmail.com>
2020-09-04 16:01:46 +0200

    [new] updateSuppresionCrashFix

--- src/main/java/carpetextra/CarpetExtraSettings.java
+++ src/main/java/carpetextra/CarpetExtraSettings.java
@@ -39,6 +39,12 @@ public class CarpetExtraSettings
     public static int scaffoldingDistance = 7;
 
     @Rule(
+            desc = "Fixes updates suppression causing server crashes.",
+            category = {BUGFIX, EXTRA}
+    )
+    public static boolean updateSuppressionCrashFix = false;
+
+    @Rule(
             desc = "Auto-crafting dropper",
             extra = {"Is a dropper points to the crafting table ",
                     "and has a valid recipe in its 3x3 it crafts it.",
