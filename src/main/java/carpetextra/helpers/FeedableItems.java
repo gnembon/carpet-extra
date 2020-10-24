effece2e7c207fa5e439140445e26ee3ed5222fb
DeadlyMC <modding.deadlymc@gmail.com>
2020-10-24 14:27:10 +0530

    Add crimson fungus to feedable items
    Fixes #140

--- src/main/java/carpetextra/helpers/FeedableItems.java
+++ src/main/java/carpetextra/helpers/FeedableItems.java
@@ -13,7 +13,7 @@ public class FeedableItems
             Items.DANDELION, Items.HAY_BLOCK, Items.WHEAT, Items.CARROT, Items.POTATO, Items.BEETROOT, Items.WHEAT_SEEDS, Items.BEETROOT_SEEDS,
             Items.MELON_SEEDS, Items.PUMPKIN_SEEDS, Items.COD, Items.SALMON, Items.ROTTEN_FLESH, Items.PORKCHOP, Items.CHICKEN, Items.RABBIT,
             Items.BEEF, Items.MUTTON, Items.COOKED_BEEF, Items.COOKED_CHICKEN, Items.COOKED_MUTTON, Items.COOKED_PORKCHOP, Items.COOKED_RABBIT,
-            Items.WARPED_FUNGUS};
+            Items.WARPED_FUNGUS, Items.CRIMSON_FUNGUS};
     
     public static final Set<Item> ITEMS = new HashSet<Item>(Arrays.asList(FEEDABLEITEMS));
 }
