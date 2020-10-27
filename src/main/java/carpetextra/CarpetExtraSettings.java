b408e983ae16228464080431f9b99ebefb2e1d08
DeadlyMC <modding.deadlymc@gmail.com>
2020-10-27 17:24:27 +0530

    Update rule descriptions
    - Added several missing full stops
    - Potentially better explain certain rules

--- src/main/java/carpetextra/CarpetExtraSettings.java
+++ src/main/java/carpetextra/CarpetExtraSettings.java
@@ -31,7 +31,7 @@ public class CarpetExtraSettings
         public String description() { return "You must choose a value from 0 to 7";}
     }
     @Rule(
-            desc="Max distance for scaffolding",
+            desc = "Max distance for scaffolding.",
             options = { "2", "3", "5", "7" },
             category = {FEATURE, EXTRA},
             strict = false,
@@ -47,28 +47,27 @@ public class CarpetExtraSettings
 
     @Rule(
             desc = "Auto-crafting dropper",
-            extra = {"Is a dropper points to the crafting table ",
-                    "and has a valid recipe in its 3x3 it crafts it.",
-                    "Overrides comparators so they indicate number of filled slots instead",
-                    "Also makes hoppers, droppers and dispensers input max 1 item per slot"
+            extra = {"If a dropper points into a crafting table and contains a valid 3x3 crafting recipe, firing that ",
+                    "dropper will cause it to craft (drop as item) that recipe. ",
+                    "Overrides comparators so they indicate number of filled slots instead.",
+                    "Also makes hoppers, droppers and dispensers input max 1 item per slot."
             },
             category = {CREATIVE, EXTRA, DISPENSER}
     )
     public static boolean autoCraftingDropper = false;
 
-    @Rule(desc="Dispensers can place blocks", category = {CREATIVE, EXTRA, DISPENSER})
+    @Rule(desc = "Dispensers can place blocks.", category = {CREATIVE, EXTRA, DISPENSER})
     public static boolean dispenserPlacesBlocks = false;
 
     @Rule(
-            desc = "variable delays on wooden components",
-            extra = "buttons, pressure plates",
+            desc = "Variable delays on wooden components (buttons, pressure plates).",
             category = {EXTRA, FEATURE}
     )
     public static boolean variableWoodDelays = false;
 
 
     @Rule(
-        desc = "Allows Comparators to read the daytime instead of the rotation of clocks in item frames",
+        desc = "Allows Comparators to read the daytime instead of the rotation of clocks in item frames.",
         category = {FEATURE,EXTRA,EXPERIMENTAL}
     )
     public static boolean comparatorReadsClock = false;
@@ -80,12 +79,12 @@ public class CarpetExtraSettings
     public static boolean hopperMinecart8gtCooldown = false;
 
     @Rule(
-        desc = "Allows Hopper Minecarts to transfer items out.",
+        desc = "Allows Hopper Minecarts to transfer items into containers below them.",
         category = {BUGFIX,FEATURE,EXTRA,EXPERIMENTAL}
     )
-    public static boolean hopperMinecartItemTransfer= false;
+    public static boolean hopperMinecartItemTransfer = false;
     
-    @Rule(desc = "Enables /ping for players to get their ping", category = {COMMAND, EXTRA})
+    @Rule(desc = "Enables `/ping` for players to get their ping.", category = {COMMAND, EXTRA})
     public static boolean commandPing = true;
     
     /*@Rule(
@@ -100,77 +99,77 @@ public class CarpetExtraSettings
     )
     public static boolean dispensersFillMinecarts = false;
     
-    //@Rule(desc = "Wet sponges dry in the nether dimension", category = {EXTRA, FEATURE})
+    //@Rule(desc = "Wet sponges dry in the nether dimension.", category = {EXTRA, FEATURE})
     //public static boolean spongesDryInTheNether = false;
 
     @Rule(
-            desc = "Clerics can warm nether farts",
-            extra = {"This will also make them pick up wart items","As well as pathfind to soulsand"},
+            desc = "Clerics can warm nether farts.",
+            extra = {"This will also allow them to pick up wart items, as well as pathfind to soulsand."},
             category = {EXTRA, FEATURE}
     )
     public static boolean clericsFarmWarts = false;
     
-    @Rule(desc = "Multiple ice crushed by falling anvils make packed ice", category = {EXTRA, EXPERIMENTAL, FEATURE})
+    @Rule(desc = "Multiple ice crushed by falling anvils make packed ice.", category = {EXTRA, EXPERIMENTAL, FEATURE})
     public static boolean renewablePackedIce = false;
     
     @Rule(
-            desc = "Dispensers can play records if there's a jukebox in front of them",
-            extra = "If record already exists in the jukebox, it gets placed back in the dispenser",
+            desc = "Dispensers can play records if there's a jukebox in front of them.",
+            extra = "If a record already exists in the jukebox, it gets placed back in the dispenser.",
             category = {EXTRA, FEATURE, DISPENSER}
     )
     public static boolean dispensersPlayRecords = false;
 
     @Rule(
-            desc = "Client can provide alternative block placement",
+            desc = "Client can provide alternative block placement.",
             category = {EXTRA, SURVIVAL}
     )
     public static boolean accurateBlockPlacement = false;
 
     @Rule(
-            desc = "Dispensers can toggle with a stick things like redstone, noteblocks, comparator, daylight detector, repeaters",
+            desc = "Dispensers containing a stick can toggle things like buttons, redstone, noteblocks, comparators, daylight detectors and repeaters.",
             category = {EXTRA, EXPERIMENTAL, FEATURE, DISPENSER}
     )
     public static boolean dispensersToggleThings = false;
 
-    @Rule(desc = "Dispensers with hoes can till soil", category = {EXTRA, FEATURE, DISPENSER})
+    @Rule(desc = "Dispensers with hoes can till soil.", category = {EXTRA, FEATURE, DISPENSER})
     public static boolean dispensersTillSoil = false;
 
-    @Rule(desc = "Dispensers can feed animals", category = {EXTRA, EXPERIMENTAL, FEATURE, DISPENSER})
+    @Rule(desc = "Dispensers can feed animals.", category = {EXTRA, EXPERIMENTAL, FEATURE, DISPENSER})
     public static boolean dispensersFeedAnimals = false;
     
-    @Rule(desc = "Disables player entity collision", category = {EXTRA, CREATIVE, EXPERIMENTAL})
+    @Rule(desc = "Disables player entity collision.", category = {EXTRA, CREATIVE, EXPERIMENTAL})
     public static boolean disablePlayerCollision = false;
     
     @Rule(
-            desc = "Obsidian surrounded by 6 lava sources has a chance of converting to lava",
+            desc = "Obsidian surrounded by 6 lava sources has a chance of converting to lava.",
             category = {EXTRA, EXPERIMENTAL, FEATURE},
             extra = "Credits: Skyrising"
     )
     public static boolean renewableLava = false;
     
     @Rule(
-            desc = "1.8 double retraction from pistons",
+            desc = "Re-adds 1.8 double retraction to pistons.",
             category = {EXTRA, EXPERIMENTAL},
-            extra = { "Gives pistons the ability to double retract without side effects" }
+            extra = { "Gives pistons the ability to double retract without side effects." }
     )
     public static boolean doubleRetraction = false;
     
     @Rule(
-            desc = "Fixes block states in F3 debug mode not updating for some blocks. May cause increased network traffic",
+            desc = "Fixes block states in F3 debug mode not updating for some blocks. May cause increased network traffic.",
             category = {EXTRA, EXPERIMENTAL},
             extra = "Works with cactus, sugar cane, saplings, hoppers, dispensers and droppers."
     )
     public static boolean blockStateSyncing = false;
     
-    @Rule(desc = "Cobblestone crushed by falling anvils makes sand", category = {EXTRA, EXPERIMENTAL, FEATURE})
+    @Rule(desc = "Cobblestone crushed by falling anvils makes sand.", category = {EXTRA, EXPERIMENTAL, FEATURE})
     public static boolean renewableSand = false;
 
 
-    @Rule(desc = "Reintroduce Dragon Egg Bedrock breaking bug from 1.12", category = {EXTRA, EXPERIMENTAL})
+    @Rule(desc = "Reintroduce the Dragon Egg Bedrock breaking bug from 1.12.", category = {EXTRA, EXPERIMENTAL})
     public static boolean dragonEggBedrockBreaking = false;
     
     @Rule(
-            desc = "Fire charges from dispensers convert cobblestone to netherrack",
+            desc = "Fire charges from dispensers convert cobblestone to netherrack.",
             category = {EXTRA, EXPERIMENTAL},
             extra = "Credits: Skyrising"
     )
@@ -179,57 +178,59 @@ public class CarpetExtraSettings
     @Rule(
             desc = "Chickens can be sheared to get feathers. Beware! every time u shear a chicken, it gets damaged!",
             category = {EXTRA, FEATURE},
-            extra = "Baby chickens can't be sheared"
+            extra = "Baby chickens can't be sheared."
     )
     public static boolean chickenShearing = false;
     
     @Rule(
-            desc = "If a living entity dies on sand with fire on top the sand will convert into soul sand",
+            desc = "If a living entity dies on sand with fire on top the sand will convert into soul sand.",
             category = {EXTRA, FEATURE, EXPERIMENTAL}
     )
     public static boolean mobInFireConvertsSandToSoulsand = false;
     
     @Rule(
-            desc = "Place a wither rose in a flowerpot to load that chunk",
-            extra = "If u enable the rule the already existing chunks with flowerpots won't be loaded. " +
-                    "Also disabling the carpet rule won't remove the loaded chunks, u need to manually unload them using the /forceload command. " +
-                    "All the loaded chunks can be seen using `/forceload query`",
+            desc = "Placing a wither rose in a flowerpot will load that chunk.",
+            extra = {
+                    "If u enable the rule the already existing chunks with flowerpots won't be loaded. ",
+                    "Also disabling the carpet rule won't remove the loaded chunks, u need to manually unload them using the /forceload command. ",
+                    "All the loaded chunks can be seen using `/forceload query`"
+            },
             category = {EXTRA, FEATURE, EXPERIMENTAL}
     )
     public static boolean flowerPotChunkLoading = false;
     
     @Rule(
             desc = "Won't let mobs glitch into blocks when reloaded.",
-            extra = "Can cause slight differences in mobs behaviour. Fixes MC-2025",
+            extra = "Can cause slight differences in mobs behaviour. Fixes [MC-2025](https://bugs.mojang.com/browse/MC-2025).",
             category = {EXTRA, BUGFIX, EXPERIMENTAL}
     )
     public static boolean reloadSuffocationFix = false;
     
     @Rule(
-            desc = "Dispensers with empty buckets can milk cows and get stew from mooshrooms with bowls",
+            desc = "Dispensers with empty buckets can milk cows and get stew from mooshrooms with bowls.",
             category = {EXPERIMENTAL, EXTRA, FEATURE, DISPENSER}
     )
     public static boolean dispensersMilkCows = false;
     
     @Rule(
-            desc = "Quick pulses won't get lost in repeater setups",
-            extra = "Probably brings back pre 1.8 behaviour. Fixes MC-54711",
+            desc = "Quick pulses won't get lost in repeater setups.",
+            extra = "Probably brings back pre 1.8 behaviour. Fixes [MC-54711](https://bugs.mojang.com/browse/MC-54711).",
             category = {EXTRA, BUGFIX, EXPERIMENTAL}
     )
     public static boolean repeaterPriorityFix = false;
     
-    @Rule(desc = "Only strays spawn in igloos", category = {FEATURE, EXTRA})
+    @Rule(desc = "Only strays spawn in igloos.", category = {FEATURE, EXTRA})
     public static boolean straySpawningInIgloos = false;
     
-    @Rule(desc = "Skeletons turn into wither skeletons when struck by lightning", category = {FEATURE, EXTRA})
+    @Rule(desc = "Skeletons turn into wither skeletons when struck by lightning.", category = {FEATURE, EXTRA})
     public static boolean renewableWitherSkeletons = false;
     
-    @Rule(desc = "Only creepers spawn in jungle temples", category = {FEATURE, EXTRA})
+    @Rule(desc = "Only creepers spawn in jungle temples.", category = {FEATURE, EXTRA})
     public static boolean creeperSpawningInJungleTemples = false;
     
     @Rule(
-            desc = "Let dragon eggs break Y0 bedrock",
-            extra = "Requires dragonEggBedrockBreaking to be set to true",
+            desc = "Let dragon eggs break Y0 bedrock.",
+            extra = "Requires dragonEggBedrockBreaking to be set to true.",
             category = {EXPERIMENTAL, EXTRA}
     )
     public static boolean y0DragonEggBedrockBreaking = false;
@@ -247,8 +248,8 @@ public class CarpetExtraSettings
     }
     
     @Rule(
-            desc = "Gives Spider jockeys a specified chance to drop enchanted golden apples",
-            extra = "0 is default, i.e no enchanted golden apples will be dropped",
+            desc = "Gives Spider jockeys a specified chance to drop enchanted golden apples.",
+            extra = "0 is the default setting, no enchanted golden apples will be dropped",
             options = {"0", "50", "100"},
             category = {EXTRA, FEATURE},
             validate = ValidateSpiderJokeyDropChance.class
@@ -256,13 +257,13 @@ public class CarpetExtraSettings
     public static int spiderJockeysDropGapples = 0;
     
     @Rule(
-            desc = "Dragon's breath from dispensers convert cobblestone to end stone",
+            desc = "Dragon's breath from dispensers convert cobblestone to end stone.",
             category = {EXTRA, EXPERIMENTAL, DISPENSER}
     )
     public static boolean dragonsBreathConvertsCobbleToEndstone = false;
 
     @Rule(
-            desc = "Maximum water sucking for sponge",
+            desc = "Maximum water sucking for sponge.",
             options = {"64"},
             category = {FEATURE, EXTRA},
             strict = false
@@ -270,7 +271,7 @@ public class CarpetExtraSettings
     public static int maxSpongeSuck = 64;
 
     @Rule(
-            desc = "Maximum offset limit for sponge",
+            desc = "Maximum offset limit for sponge.",
             options = {"7"},
             category = {FEATURE, EXTRA},
             strict = false
@@ -278,30 +279,30 @@ public class CarpetExtraSettings
     public static int maxSpongeRange = 7;
 
     @Rule(
-            desc = "Empty Shulker Boxes will always stack, even inside inventories",
+            desc = "Empty Shulker Boxes will always stack, even inside inventories.",
             category = {FEATURE, EXTRA, EXPERIMENTAL}
     )
     public static boolean emptyShulkerBoxStackAlways = false;
 
     @Rule(
-            desc = "Allow horizontally moving Ender Pearls to load chunks as entity ticking",
+            desc = "Allow horizontally moving Ender Pearls to load chunks as entity ticking.",
             category = {FEATURE, EXTRA}
     )
     public static boolean enderPearlChunkLoading = false;
 
     @Rule(
-            desc = "Bonemeal can be used to grow sugarcane and cactus",
+            desc = "Bonemeal can be used to grow sugarcane and cactus.",
             category = {FEATURE, EXTRA, SURVIVAL}
     )
     public static boolean betterBonemeal = false;
 
     @Rule(
-            desc = "Dispensers with shears can carve pumpkins",
+            desc = "Dispensers containing shears can carve pumpkins.",
             category = {EXTRA, FEATURE, DISPENSER}
     )
     public static boolean dispensersCarvePumpkins = false;
     
-    @Rule(desc = "Blaze powder fertilizes netherwart via dispenser or player right click action", category = {EXTRA, FEATURE, SURVIVAL})
+    @Rule(desc = "Blaze powder fertilizes netherwart via dispenser or player right click actions.", category = {EXTRA, FEATURE, SURVIVAL})
     public static boolean blazeMeal = false;
 
     // Implemented in 1.15 - doInsomnia
