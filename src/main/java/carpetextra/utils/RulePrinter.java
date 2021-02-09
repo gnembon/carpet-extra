e8a2b72e53eeaf4ba953e42eae6f11441fcab13b
altrisi <altrisi.trillosierra@gmail.com>
2021-02-09 18:37:23 +0100

    Actually make Rules-to-Readme work with the new system

--- src/main/java/carpetextra/utils/RulePrinter.java
+++ src/main/java/carpetextra/utils/RulePrinter.java
@@ -3,12 +3,12 @@ package carpetextra.utils;
 import carpetextra.CarpetExtraServer;
 import carpet.CarpetServer;
 import carpet.utils.CarpetRulePrinter;
-import net.fabricmc.api.DedicatedServerModInitializer;
+import net.fabricmc.api.ModInitializer;
 import java.lang.System;
 
-public class RulePrinter implements DedicatedServerModInitializer {
+public class RulePrinter implements ModInitializer {
     @Override
-    public void onInitializeServer() {
+    public void onInitialize() {
         System.setOut(CarpetRulePrinter.OLD_OUT);
         CarpetExtraServer.noop();
         CarpetServer.onGameStarted();
