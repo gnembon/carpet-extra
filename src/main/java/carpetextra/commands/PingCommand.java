bb0b37884518a03c63c86e9716629a55ebd817ef
altrisi <altrisi.trillosierra@gmail.com>
2022-05-09 15:41:30 +0200

    22w18a and maven back to masa's

--- src/main/java/carpetextra/commands/PingCommand.java
+++ src/main/java/carpetextra/commands/PingCommand.java
@@ -6,7 +6,6 @@ import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
-import net.minecraft.util.Util;
 
 import static net.minecraft.server.command.CommandManager.literal;
 
@@ -20,7 +19,7 @@ public class PingCommand
                         {
                             ServerPlayerEntity playerEntity = c.getSource().getPlayer();
                             int ping = playerEntity.pingMilliseconds;
-                            playerEntity.sendSystemMessage(Text.literal("Your ping is: " + ping + " ms"), Util.NIL_UUID);
+                            c.getSource().sendFeedback(Text.literal("Your ping is: " + ping + " ms"), false);
                             return 1;
                         });
         
