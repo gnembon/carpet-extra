a6f7763bd130397c2fd680a870ccc521d7b77f53
altrisi <altrisi.trillosierra@gmail.com>
2022-05-09 19:58:01 +0200

    Add Scarpet event to dispenser actions

--- src/main/java/carpetextra/mixins/DispenserBlock_GetCustomBehaviorMixin.java
+++ src/main/java/carpetextra/mixins/DispenserBlock_GetCustomBehaviorMixin.java
@@ -10,7 +10,10 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
+import carpet.script.value.Value;
+import carpet.script.value.ValueConversions;
 import carpetextra.dispenser.CarpetExtraDispenserBehaviors;
+import carpetextra.dispenser.DispenserEvent;
 import net.minecraft.block.DispenserBlock;
 import net.minecraft.block.dispenser.DispenserBehavior;
 import net.minecraft.block.entity.DispenserBlockEntity;
@@ -33,14 +36,21 @@ public abstract class DispenserBlock_GetCustomBehaviorMixin {
         locals = LocalCapture.CAPTURE_FAILHARD,
         cancellable = true
     )
-    private void dispenseCustomBehavior(ServerWorld serverWorld, BlockPos pos, CallbackInfo ci, BlockPointerImpl blockPointerImpl, DispenserBlockEntity dispenserBlockEntity, int i, ItemStack itemStack) {
+    private void dispenseCustomBehavior(ServerWorld serverWorld, BlockPos pos, CallbackInfo ci, BlockPointerImpl blockPointer, DispenserBlockEntity dispenserBlockEntity, int i, ItemStack itemStack) {
         // get custom behavior
-        DispenserBehavior customBehavior = CarpetExtraDispenserBehaviors.getCustomDispenserBehavior(serverWorld, pos, blockPointerImpl, dispenserBlockEntity, itemStack, BEHAVIORS);
+        DispenserBehavior behavior = CarpetExtraDispenserBehaviors.getCustomDispenserBehavior(serverWorld, pos, blockPointer, dispenserBlockEntity, itemStack, BEHAVIORS);
 
         // check if custom behavior exists
-        if(customBehavior != null) {
+        if (behavior != null) {
             // run custom behavior
-            dispenserBlockEntity.setStack(i, customBehavior.dispense(blockPointerImpl, itemStack));
+            Value previousStackSnapshot = null;
+            if (DispenserEvent.needed()) previousStackSnapshot = ValueConversions.of(itemStack);
+
+            ItemStack resultStack = behavior.dispense(blockPointer, itemStack);
+            dispenserBlockEntity.setStack(i, resultStack);
+
+            DispenserEvent.call(blockPointer, behavior, pos, previousStackSnapshot, resultStack);
+
             ci.cancel();
         }
     }
