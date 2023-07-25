f91d5aa4728c5b1f3c8492e68a5e90957965fbb7
Ghoulboy <ghoulboy27@gmail.com>
2023-07-25 15:37:12 +0200

    Add xpPerSculkCatalyst rule (#274)

--- src/main/java/carpetextra/CarpetExtraSettings.java
+++ src/main/java/carpetextra/CarpetExtraSettings.java
@@ -3,6 +3,7 @@ package carpetextra;
 import carpet.api.settings.Rule;
 import carpet.api.settings.CarpetRule;
 import carpet.api.settings.Validator;
+import carpet.api.settings.Validators;
 import net.minecraft.server.command.ServerCommandSource;
 
 import static carpet.api.settings.RuleCategory.*;
@@ -271,4 +272,12 @@ public class CarpetExtraSettings
             categories = {EXTRA, FEATURE, DISPENSER}
     )
     public static boolean fallingBlockDispensers = false;
+
+    @Rule(
+            options = {"5", "10", "20"},
+            strict = false,
+            categories = {EXTRA, FEATURE},
+            validators = Validators.NonNegativeNumber.class
+    )
+    public static int xpPerSculkCatalyst = 5;
 }
