092ee7e758d8e14fa0751f749bfb2e690a0ecae0
altrisi <altrisi.trillosierra@gmail.com>
2023-07-25 19:13:43 +0200

    Fix crash with `dispensersToggleThings` introduced with mob heads

--- src/main/java/carpetextra/mixins/NoteBlockMixin.java
+++ src/main/java/carpetextra/mixins/NoteBlockMixin.java
@@ -2,6 +2,8 @@ package carpetextra.mixins;
 
 import net.minecraft.block.NoteBlock;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -22,4 +24,17 @@ public abstract class NoteBlockMixin {
         player.incrementStat(ident);
         return;
     }
+
+    @Redirect(
+            method = "onUse",
+            at = @At(
+                value = "INVOKE",
+                target = "net/minecraft/entity/player/PlayerEntity.getStackInHand(Lnet/minecraft/util/Hand;)Lnet/minecraft/item/ItemStack;"
+            )
+        )
+        private ItemStack onGetItemInHand(PlayerEntity player, Hand hand) {
+            // player will never be null in VANILLA
+            if (player == null) return ItemStack.EMPTY;
+            return player.getStackInHand(hand);
+        }
 }
