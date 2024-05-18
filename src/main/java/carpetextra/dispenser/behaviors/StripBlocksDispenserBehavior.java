897f6d3c1fcab3c767d5afee0553537ac4664a61
Vasiliy <void.pointer@ya.ru>
2024-05-19 01:34:34 +0500

    fix(1.20.6): adjust `#damage` method, fix grammar

--- src/main/java/carpetextra/dispenser/behaviors/StripBlocksDispenserBehavior.java
+++ src/main/java/carpetextra/dispenser/behaviors/StripBlocksDispenserBehavior.java
@@ -1,8 +1,5 @@
 package carpetextra.dispenser.behaviors;
 
-import java.util.Collection;
-import java.util.Set;
-
 import carpetextra.dispenser.DispenserItemUsageContext;
 import carpetextra.mixins.AxeItem_StrippedBlocksAccessorMixin;
 import net.minecraft.block.Block;
@@ -19,6 +16,9 @@ import net.minecraft.util.math.BlockPointer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
+
+import java.util.Collection;
+import java.util.Set;
 
 public class StripBlocksDispenserBehavior extends FallibleItemDispenserBehavior {
     // stripable blocks
@@ -49,9 +49,7 @@ public class StripBlocksDispenserBehavior extends FallibleItemDispenserBehavior 
             // use on block, test if sucessful
             if(stack.getItem().useOnBlock(context).isAccepted()) {
                 // damage axe, remove if broken
-                if(stack.damage(1, world.random, null)) {
-                    stack.setCount(0);
-                }
+                stack.damage(1, world.random, null, () -> stack.setCount(0));
 
                 return stack;
             }
@@ -62,7 +60,7 @@ public class StripBlocksDispenserBehavior extends FallibleItemDispenserBehavior 
         return stack;
     }
 
-    // checks if a block is able to be stripped with an axe
+    // checks if a block can be stripped with an axe
     public static boolean canStrip(Block block) {
         return STRIPPED_BLOCKS.contains(block) || DEOXIDIZE_BLOCKS.contains(block) || DEWAX_BLOCKS.contains(block);
     }
