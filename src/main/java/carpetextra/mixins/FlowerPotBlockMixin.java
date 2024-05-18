0230891270062042080e6298bc8c1ac5d472468d
Vasiliy <void.pointer@ya.ru>
2024-05-19 02:22:14 +0500

    fix(1.20.6): adjust method parameters

--- src/main/java/carpetextra/mixins/FlowerPotBlockMixin.java
+++ src/main/java/carpetextra/mixins/FlowerPotBlockMixin.java
@@ -1,5 +1,14 @@
 package carpetextra.mixins;
 
+import carpetextra.helpers.FlowerPotHelper;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.FlowerPotBlock;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -7,19 +16,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
-
-import carpetextra.helpers.FlowerPotHelper;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.FlowerPotBlock;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.util.hit.BlockHitResult;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 
 @Mixin(FlowerPotBlock.class)
 public abstract class FlowerPotBlockMixin extends Block
@@ -40,7 +36,7 @@ public abstract class FlowerPotBlockMixin extends Block
         ),
         locals = LocalCapture.CAPTURE_FAILHARD
     )
-    private void onActivate(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit, CallbackInfoReturnable<ActionResult> cir, ItemStack itemStack, Item i, BlockState blockState) {
+    private void onActivate(BlockState blockState, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit, CallbackInfoReturnable<ActionResult> cir) {
         // check if chunk should add force load when flower is placed in pot
         FlowerPotHelper.updateLoadStatus(world, pos, ((FlowerPotBlock) blockState.getBlock()).getContent(), true);
     }
