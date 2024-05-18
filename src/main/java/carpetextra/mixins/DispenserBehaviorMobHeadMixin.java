647ef62b68c1cf0df92e0b2c6c27f3b5ab7fc219
Vasiliy <void.pointer@ya.ru>
2024-05-19 01:44:47 +0500

    fix(1.20.6): adjust bytecode anonymous class ID for mob heads

--- src/main/java/carpetextra/mixins/DispenserBehaviorMobHeadMixin.java
+++ src/main/java/carpetextra/mixins/DispenserBehaviorMobHeadMixin.java
@@ -1,17 +1,16 @@
 package carpetextra.mixins;
 
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
 import carpetextra.utils.PlaceBlockDispenserBehavior;
 import net.minecraft.block.dispenser.FallibleItemDispenserBehavior;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPointer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(targets = {"net/minecraft/block/dispenser/DispenserBehavior$14", "net/minecraft/block/dispenser/DispenserBehavior$15"})
+@Mixin(targets = {"net/minecraft/block/dispenser/DispenserBehavior$3"})
 public abstract class DispenserBehaviorMobHeadMixin extends FallibleItemDispenserBehavior {
     @SuppressWarnings("UnresolvedMixinReference")
     @Inject(
