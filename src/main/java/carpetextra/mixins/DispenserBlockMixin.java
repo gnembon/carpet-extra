fd694958377ca721a5c12d17e5e7838f0d07a8b2
Vasiliy <void.pointer@ya.ru>
2024-05-19 02:09:45 +0500

    fix(1.20.6): adjust method parameters

--- src/main/java/carpetextra/mixins/DispenserBlockMixin.java
+++ src/main/java/carpetextra/mixins/DispenserBlockMixin.java
@@ -1,13 +1,16 @@
 package carpetextra.mixins;
 
 import carpetextra.CarpetExtraSettings;
+import carpetextra.utils.PlaceBlockDispenserBehavior;
 import net.minecraft.block.DispenserBlock;
 import net.minecraft.block.dispenser.DispenserBehavior;
-import net.minecraft.item.*;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import carpetextra.utils.PlaceBlockDispenserBehavior;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -22,7 +25,7 @@ public abstract class DispenserBlockMixin
     // this is old code to keep dispenserPlacesBlocks working as is
     // new behaviors should go in CarpetExtraDispenserBehaviors class
     @Inject(method = "getBehaviorForItem", at = @At("HEAD"), cancellable = true)
-    private void getBehaviorForItem(ItemStack stack, CallbackInfoReturnable<DispenserBehavior> cir)
+    private void getBehaviorForItem(World world, ItemStack stack, CallbackInfoReturnable<DispenserBehavior> cir)
     {
         Item item = stack.getItem();
         if (CarpetExtraSettings.dispenserPlacesBlocks && !BEHAVIORS.containsKey(item) && item instanceof BlockItem)
