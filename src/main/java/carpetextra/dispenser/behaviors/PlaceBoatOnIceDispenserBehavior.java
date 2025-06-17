b897eb2f127051e690f46f041c4a2f4168b11102
altrisi <altrisi.trillosierra@gmail.com>
2025-06-17 20:20:25 +0200

    Fix boats not dropping if placed by dispensersPlaceBoatsOnIce
    By aligning with BoatDispenserBehaviour.
    Pending gametest.

--- src/main/java/carpetextra/dispenser/behaviors/PlaceBoatOnIceDispenserBehavior.java
+++ src/main/java/carpetextra/dispenser/behaviors/PlaceBoatOnIceDispenserBehavior.java
@@ -1,6 +1,5 @@
 package carpetextra.dispenser.behaviors;
 
-import java.util.function.Supplier;
 import carpetextra.mixins.BoatItemAccessorMixin;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
@@ -8,17 +7,15 @@ import net.minecraft.block.Blocks;
 import net.minecraft.block.DispenserBlock;
 import net.minecraft.block.dispenser.FallibleItemDispenserBehavior;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.vehicle.AbstractBoatEntity;
-import net.minecraft.entity.vehicle.AbstractChestBoatEntity;
-import net.minecraft.entity.vehicle.BoatEntity;
-import net.minecraft.entity.vehicle.ChestBoatEntity;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPointer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
 
 public class PlaceBoatOnIceDispenserBehavior extends FallibleItemDispenserBehavior {
 
@@ -26,38 +23,28 @@ public class PlaceBoatOnIceDispenserBehavior extends FallibleItemDispenserBehavi
     public ItemStack dispenseSilently(BlockPointer pointer, ItemStack stack) {
         this.setSuccess(true);
         ServerWorld world = pointer.world();
-        Direction dispenserFacing = pointer.state().get(DispenserBlock.FACING);
+        Direction facing = pointer.state().get(DispenserBlock.FACING);
+        
+        BoatItemAccessorMixin boatInfo = (BoatItemAccessorMixin) stack.getItem();
+        EntityType<? extends AbstractBoatEntity> boatType = boatInfo.getType();
 
-        double boatXPos = pointer.pos().getX() + (double) ((float) dispenserFacing.getOffsetX() * 1.125F);
-        double boatYPos = pointer.pos().getY() + (double) ((float) dispenserFacing.getOffsetY() * 1.125F);
-        double boatZPos = pointer.pos().getZ() + (double) ((float) dispenserFacing.getOffsetZ() * 1.125F);
+        Vec3d vec3d = pointer.centerPos();
+        double d = 0.5625 + boatType.getWidth() / 2.0;
+        double x = vec3d.getX() + facing.getOffsetX() * d;
+        double y = vec3d.getY() + facing.getOffsetY() * 1.125F;
+        double z = vec3d.getZ() + facing.getOffsetZ() * d;
 
-        BlockPos frontBlockPos = pointer.pos().offset(dispenserFacing);
+        BlockPos frontBlockPos = pointer.pos().offset(facing);
         Block frontBlock = world.getBlockState(frontBlockPos).getBlock();
-        BlockPos blockBelowFrontBlockPos = frontBlockPos.down();
-        BlockState blockBelowFrontState = world.getBlockState(blockBelowFrontBlockPos);
+        BlockState stateBelowFront = world.getBlockState(frontBlockPos.down());
 
-        if (frontBlock == Blocks.AIR && blockBelowFrontState.isIn(BlockTags.ICE)) {
-            BoatItemAccessorMixin boatInfo = (BoatItemAccessorMixin) stack.getItem();
-            EntityType<? extends AbstractBoatEntity> boatType = boatInfo.getType();
-            AbstractBoatEntity boatEntity;
+        if (frontBlock == Blocks.AIR && stateBelowFront.isIn(BlockTags.ICE)) {
+            AbstractBoatEntity boatEntity = boatType.create(world, SpawnReason.DISPENSER);
 
-            if (this.isChestBoat(boatType)) {
-                boatEntity = new ChestBoatEntity((EntityType<? extends ChestBoatEntity>) boatType, world, stack::getItem);
-            } else {
-                boatEntity = new BoatEntity((EntityType<? extends BoatEntity>) boatType, world, stack::getItem);
-            }
-            boatEntity.initPosition(boatXPos, boatYPos, boatZPos);
-            /*
-            if (boatInfo.isChest()) {
-                boatEntity = new ChestBoatEntity(world, boatXPos, boatYPos, boatZPos);
-            } else {
-                boatEntity = new BoatEntity(world, boatXPos, boatYPos, boatZPos);
-            }
-            boatEntity.setVariant(boatInfo.getType());
-             */
+            boatEntity.initPosition(x, y, z);
+            EntityType.copier(world, stack, null).accept(boatEntity);
+            boatEntity.setYaw(facing.getPositiveHorizontalDegrees());
 
-            boatEntity.setYaw(dispenserFacing.getPositiveHorizontalDegrees());
             world.spawnEntity(boatEntity);
             stack.decrement(1);
             return stack;
@@ -66,19 +53,5 @@ public class PlaceBoatOnIceDispenserBehavior extends FallibleItemDispenserBehavi
         // fail to dispense
         this.setSuccess(false);
         return stack;
-    }
-
-    private boolean isChestBoat(EntityType<? extends AbstractBoatEntity> boatType)
-    {
-        if      (boatType.equals(EntityType.ACACIA_CHEST_BOAT))     { return true; }
-        else if (boatType.equals(EntityType.OAK_CHEST_BOAT))        { return true; }
-        else if (boatType.equals(EntityType.MANGROVE_CHEST_BOAT))   { return true; }
-        else if (boatType.equals(EntityType.JUNGLE_CHEST_BOAT))     { return true; }
-        else if (boatType.equals(EntityType.CHERRY_CHEST_BOAT))     { return true; }
-        else if (boatType.equals(EntityType.SPRUCE_CHEST_BOAT))     { return true; }
-        else if (boatType.equals(EntityType.BIRCH_CHEST_BOAT))      { return true; }
-        else if (boatType.equals(EntityType.DARK_OAK_CHEST_BOAT))   { return true; }
-        else if (boatType.equals(EntityType.BAMBOO_CHEST_RAFT))     { return true; }
-        else return boatType.equals(EntityType.PALE_OAK_CHEST_BOAT);
     }
 }
