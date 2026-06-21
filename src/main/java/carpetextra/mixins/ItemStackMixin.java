1520e267cab0c75ec4acd4981942075d25ee464b
altrisi <altrisi.trillosierra@gmail.com>
2026-06-21 12:07:25 +0200

    Handle other vanilla ItemStackView subclass

--- src/main/java/carpetextra/mixins/ItemStackMixin.java
+++ src/main/java/carpetextra/mixins/ItemStackMixin.java
@@ -11,15 +11,19 @@ import net.minecraft.block.ShulkerBoxBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemStackView;
+import net.minecraft.item.SimpleItemStackView;
 import carpet.CarpetSettings;
 
 @Mixin(ItemStackView.class)
 public interface ItemStackMixin {
     @Inject(method = "getMaxCount", at = @At("HEAD"), cancellable = true)
     private void allowEmptyShulkerBoxStacking(CallbackInfoReturnable<Integer> cir) {
-        // TODO do we need to handle non-ItemStack versions?
-        if ((Object)this instanceof ItemStack thisStack && CarpetExtraSettings.emptyShulkerBoxStackAlways && thisStack.getItem() instanceof BlockItem item) {
-            //noinspection UnreachableCode
+        ItemStack thisStack = switch ((ItemStackView)this) {
+            case ItemStack is -> is;
+            case SimpleItemStackView sisv -> sisv.toItemStack();
+            default -> null; // for now, we don't handle arbitrary (modded) subclasses
+        };
+        if (thisStack != null && CarpetExtraSettings.emptyShulkerBoxStackAlways && thisStack.getItem() instanceof BlockItem item) {
             if (item.getBlock() instanceof ShulkerBoxBlock && !InventoryUtils.shulkerBoxHasItems(thisStack)) {
                 cir.setReturnValue(CarpetSettings.shulkerBoxStackSize);
             }
